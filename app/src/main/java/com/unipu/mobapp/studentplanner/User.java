4ceb3dd7465861eb33e552044cae5020b4d85d0f
ktrn <katarina.lusicic@gmail.com>
2021-03-07 11:02:51 +0100

    initial commit, signup

---
+++ app/src/main/java/com/unipu/mobapp/studentplanner/User.java
@@ -0,0 +1,14 @@
+package com.unipu.mobapp.studentplanner;
+
+public class User {
+
+    public String email;
+
+    public User(){
+
+    }
+
+    public User(String email){
+        this.email = email;
+    }
+}
