fc678bd62eca6a04bb6e53fb1478b000bd14e966
Monika <monika.lenic1@gmail.com>
2021-03-24 23:21:16 +0100

    EditCourseActivity, delete on course

--- app/src/main/java/com/unipu/mobapp/studentplanner/EditCourseActivity.java
+++ app/src/main/java/com/unipu/mobapp/studentplanner/EditCourseActivity.java
@@ -1,16 +1,28 @@
 package com.unipu.mobapp.studentplanner;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class EditCourseActivity extends AppCompatActivity {
 
     EditText editTitle, editColloquium;
     Button btnEditSave, btnDelete;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +37,25 @@ public class EditCourseActivity extends AppCompatActivity {
         //get a value
         editTitle.setText(getIntent().getStringExtra("courseName"));
         editColloquium.setText(getIntent().getStringExtra("examNum"));
-}
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Course").child("Course-204719274");
+                reference.removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(EditCourseActivity.this, "Deleted", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(EditCourseActivity.this, CoursesActivity.class);
+                            startActivity(intent);
+
+                        }else{
+                            Toast.makeText(EditCourseActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+  }
 }
