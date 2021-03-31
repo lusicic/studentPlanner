d1f9ddba8e37128da398ec8fa5f0e6b48f212f4e
Monika <monika.lenic1@gmail.com>
2021-03-31 10:07:21 +0200

    edit and saveEdit to base (DatePicker)

--- app/src/main/java/com/unipu/mobapp/studentplanner/EditNotesActivity.java
+++ app/src/main/java/com/unipu/mobapp/studentplanner/EditNotesActivity.java
@@ -1,11 +1,14 @@
 package com.unipu.mobapp.studentplanner;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,20 +24,47 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Calendar;
+
 
 public class EditNotesActivity extends AppCompatActivity {
 
     EditText editTitle, editDate, editDescr;
     Button btnEditSave, btnDelete;
     DatabaseReference databaseReference;
+    TextView addDateCalendar;
+    private DatePickerDialog.OnDateSetListener onDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_notes);
 
-        editTitle = findViewById(R.id.editTitle);
+        addDateCalendar = (TextView) findViewById(R.id.titleDate);
+        addDateCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(EditNotesActivity.this,
+                        android.R.style.Theme_Material_Dialog_NoActionBar,
+                        onDateSetListener, year, month, day);
+                datePickerDialog.getWindow();
+                datePickerDialog.show();
+            }
+        });
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "/" + (month+1) + "/" + year;
+                editDate.setText(date);
+            }
+        };
+
         editDate = findViewById(R.id.editDate);
+        editTitle = findViewById(R.id.editTitle);
         editDescr = findViewById(R.id.editDescr);
 
         btnEditSave = findViewById(R.id.btnCreate);
