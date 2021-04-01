7c7b2f4e0f442d62c07680af5ee40474e795aa0b
Simona <simonaj0410@gmail.com>
2021-04-01 12:06:53 +0200

    edit tAsk

--- app/src/main/java/com/unipu/mobapp/studentplanner/TaskEdit.java
+++ app/src/main/java/com/unipu/mobapp/studentplanner/TaskEdit.java
@@ -3,11 +3,14 @@ package com.unipu.mobapp.studentplanner;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,14 +23,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Calendar;
 import java.util.Random;
 
 public class TaskEdit extends AppCompatActivity {
+
 
     EditText taskName, grade, descript, editDate;
     Button btnEdit, buttonDelete;
     DatabaseReference mBase;
     TaskAdapter adapter;
+    TextView addDateCalendar;
+    private DatePickerDialog.OnDateSetListener onDateSetListener;
 
     Integer brojac = new Random().nextInt();
     String keytask = Integer.toString(brojac);
@@ -37,13 +44,46 @@ public class TaskEdit extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_task);
 
+        addDateCalendar = (TextView) findViewById(R.id.addDate);
+        addDateCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(TaskEdit.this,
+                        android.R.style.Theme_Material_Dialog_NoActionBar,
+                        onDateSetListener, year, month, day);
+                datePickerDialog.getWindow();
+                datePickerDialog.show();
+            }
+        });
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "/" + (month+1) + "/" + year;
+                editDate.setText(date);
+            }
+        };
+
         taskName = findViewById(R.id.taskName);
         grade = findViewById(R.id.grade);
         descript = findViewById(R.id.descript);
-        editDate = findViewById(R.id.editDate);
+        editDate =  findViewById(R.id.editDate);
+
 
         btnEdit = findViewById(R.id.btnEdit);
         buttonDelete = findViewById(R.id.buttonDelete);
+
+
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "/" + (month+1) + "/" + year;
+                editDate.setText(date);
+            }
+        };
 
         //get a value
         taskName.setText(getIntent().getStringExtra("taskName"));
