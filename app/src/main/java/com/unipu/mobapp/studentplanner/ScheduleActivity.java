d42abffda05be4e6f19fd977dd22c9d14b08976b
ktrn <katarina.lusicic@gmail.com>
2021-04-05 12:18:58 +0200

    image retrieving for schedule

--- app/src/main/java/com/unipu/mobapp/studentplanner/ScheduleActivity.java
+++ app/src/main/java/com/unipu/mobapp/studentplanner/ScheduleActivity.java
@@ -6,26 +6,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ScheduleActivity extends AppCompatActivity {
 
     private Button btnAddSchedule;
     private Uri imageUri;
     private static final int IMAGE_REQUEST = 2;
+
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +51,28 @@ public class ScheduleActivity extends AppCompatActivity {
                 openSchedule();
             }
         });
+
+        mStorageRef = FirebaseStorage.getInstance().getReference().child("uploads/").child(uid).child("schedule.jpg");
+
+        try {
+            final File localFile = File.createTempFile("schedule", "jpg");
+            mStorageRef.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            ((ImageView)findViewById(R.id.imageView)).setImageBitmap(bitmap);
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(ScheduleActivity.this, "Image not retrieved", Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void openSchedule() {
@@ -67,10 +101,16 @@ public class ScheduleActivity extends AppCompatActivity {
         return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
     }
 
+    final FirebaseAuth auth = FirebaseAuth.getInstance();
+    final FirebaseUser usery = auth.getCurrentUser();
+    final String uid = usery.getUid();
+
+
+    // System.currentTimeMillis()
     private void uploadSchedule() {
 
         if(imageUri != null){
-            final StorageReference fileRef = FirebaseStorage.getInstance().getReference().child("uploads").child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+            final StorageReference fileRef = FirebaseStorage.getInstance().getReference().child("uploads").child(uid).child("schedule" + "." + getFileExtension(imageUri));
 
             fileRef.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                 @Override
